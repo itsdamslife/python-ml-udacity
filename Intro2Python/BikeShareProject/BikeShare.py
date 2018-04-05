3fd0ee596ff661fb5573acb6e305704784f116e1
Damodar Shenoy <itsdamslife@gmail.com>
2018-04-05 23:28:22 +0530

    Condensed data - creates summary files and writes to it

--- Intro2Python/BikeShareProject/BikeShare.py
+++ Intro2Python/BikeShareProject/BikeShare.py
@@ -165,3 +165,61 @@ for city in tests:
     usr_typ = type_of_user(example_trips[city], city)
     print("============={}".format(usr_typ)) # debug
     assert usr_typ == tests[city]
+
+# Condense data
+
+
+def condense_data(in_file, out_file, city):
+    """
+    This function takes full data from the specified input file
+    and writes the condensed data to a specified output file. The city
+    argument determines how the input file will be parsed.
+    
+    HINT: See the cell below to see how the arguments are structured!
+    """
+
+    with open(out_file, 'w') as f_out, open(in_file, 'r') as f_in:
+        # set up csv DictWriter object - writer requires column names for the
+        # first row as the "fieldnames" argument
+        out_colnames = ['duration', 'month',
+                        'hour', 'day_of_week', 'user_type']
+        trip_writer = csv.DictWriter(f_out, fieldnames=out_colnames)
+        trip_writer.writeheader()
+
+        ## TODO: set up csv DictReader object ##
+        trip_reader = csv.DictReader(f_in)
+
+        # collect data from and process each row
+        for row in trip_reader:
+            # set up a dictionary to hold the values for the cleaned and trimmed
+            # data point
+            new_point = {}
+            
+            ## TODO: use the helper functions to get the cleaned data from  ##
+            ## the original data dictionaries.                              ##
+            ## Note that the keys for the new_point dictionary should match ##
+            ## the column names set in the DictWriter object above.         ##
+            month_hour_day = time_of_trip(row, city)
+            new_point['duration'] = duration_in_mins(row, city)
+            new_point['month'] = month_hour_day[0]
+            new_point['hour'] = month_hour_day[1]
+            new_point['day_of_week'] = month_hour_day[2]
+            new_point['user_type'] = type_of_user(row, city)
+            
+            ## TODO: write the processed information to the output file.     ##
+            ## see https://docs.python.org/3/library/csv.html#writer-objects ##
+            trip_writer.writerow(new_point)
+        
+        f_out.close()
+
+# Run this to check your work
+city_info = {'Washington': {'in_file': './data/Washington-CapitalBikeshare-2016.csv',
+                            'out_file': './data/Washington-2016-Summary.csv'},
+             'Chicago': {'in_file': './data/Chicago-Divvy-2016.csv',
+                         'out_file': './data/Chicago-2016-Summary.csv'},
+             'NYC': {'in_file': './data/NYC-CitiBike-2016.csv',
+                     'out_file': './data/NYC-2016-Summary.csv'}}
+
+for city, filenames in city_info.items():
+    condense_data(filenames['in_file'], filenames['out_file'], city)
+    print_first_point(filenames['out_file'])
